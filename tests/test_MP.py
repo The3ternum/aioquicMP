bff3f327cd8886c06001a670ef4b163527d51646
Tim Mesotten <mesotten.tim@gmail.com>
2020-07-21 00:46:39 +0200

    Added test_MP

---
+++ tests/test_MP.py
@@ -0,0 +1,227 @@
+import asyncio
+import contextlib
+import time
+from unittest import TestCase
+
+from aioquic import tls
+from aioquic.quic import events
+from aioquic.quic.configuration import QuicConfiguration
+from aioquic.quic.connection import QuicConnection, QuicReceiveContext
+from aioquic.quic.logger import QuicLogger
+from aioquic.quic.packet import QuicErrorCode
+from aioquic.quic.recovery import QuicPacketPacer
+from tests.utils import SERVER_CACERTFILE, SERVER_CERTFILE, SERVER_KEYFILE
+
+CLIENT_ADDR = ("1.2.3.4", 1234)
+
+SERVER_ADDR = ("2.3.4.5", 4433)
+
+
+class SessionTicketStore:
+    def __init__(self):
+        self.tickets = {}
+
+    def add(self, ticket):
+        self.tickets[ticket.ticket] = ticket
+
+    def pop(self, label):
+        return self.tickets.pop(label, None)
+
+
+def client_receive_context(client, epoch=tls.Epoch.ONE_RTT):
+    return QuicReceiveContext(
+        epoch=epoch,
+        host_cid=client._receiving_uniflows[0].cid,
+        network_path=client._network_paths[0],
+        quic_logger_frames=[],
+        time=asyncio.get_event_loop().time(),
+    )
+
+
+def consume_events(connection):
+    while True:
+        event = connection.next_event()
+        if event is None:
+            break
+
+
+def create_standalone_client(self, **client_options):
+    client = QuicConnection(
+        configuration=QuicConfiguration(
+            is_client=True, quic_logger=QuicLogger(), **client_options
+        )
+    )
+    client._ack_delay = 0
+
+    # kick-off handshake
+    client.connect(SERVER_ADDR, now=time.time())
+    self.assertEqual(drop(client), 1)
+
+    return client
+
+
+@contextlib.contextmanager
+def client_and_server(
+    client_kwargs={},
+    client_options={},
+    client_patch=lambda x: None,
+    handshake=True,
+    server_kwargs={},
+    server_certfile=SERVER_CERTFILE,
+    server_keyfile=SERVER_KEYFILE,
+    server_options={},
+    server_patch=lambda x: None,
+):
+    client_configuration = QuicConfiguration(
+        is_client=True,
+        quic_logger=QuicLogger(),
+        max_sending_uniflow_id=1,
+        **client_options
+    )
+    client_configuration.load_verify_locations(cafile=SERVER_CACERTFILE)
+
+    client = QuicConnection(configuration=client_configuration, **client_kwargs)
+    client._ack_delay = 0
+    disable_packet_pacing(client)
+    client_patch(client)
+
+    server_configuration = QuicConfiguration(
+        is_client=False,
+        quic_logger=QuicLogger(),
+        max_sending_uniflow_id=1,
+        **server_options
+    )
+    server_configuration.load_cert_chain(server_certfile, server_keyfile)
+
+    server = QuicConnection(configuration=server_configuration, **server_kwargs)
+    server._ack_delay = 0
+    disable_packet_pacing(server)
+    server_patch(server)
+
+    # perform handshake
+    if handshake:
+        client.connect(SERVER_ADDR, now=time.time())
+        for i in range(3):
+            roundtrip(client, server)
+
+    yield client, server
+
+    # close
+    client.close()
+    server.close()
+
+
+def disable_packet_pacing(connection):
+    class DummyPacketPacer(QuicPacketPacer):
+        def next_send_time(self, now):
+            return None
+
+    connection._loss._pacer = DummyPacketPacer()
+
+
+def sequence_numbers(connection_ids):
+    return list(map(lambda x: x.sequence_number, connection_ids))
+
+
+def drop(sender):
+    """
+    Drop datagrams from `sender`.
+    """
+    return len(sender.datagrams_to_send(now=time.time()))
+
+
+def roundtrip(sender, receiver):
+    """
+    Send datagrams from `sender` to `receiver` and back.
+    """
+    return transfer(sender, receiver), transfer(receiver, sender)
+
+
+def transfer(sender, receiver):
+    """
+    Send datagrams from `sender` to `receiver`.
+    """
+    datagrams = 0
+    from_addr = CLIENT_ADDR if sender._is_client else SERVER_ADDR
+    for data, addr in sender.datagrams_to_send(now=time.time()):
+        datagrams += 1
+        receiver.receive_datagram(data, from_addr, now=time.time())
+    return datagrams
+
+
+class QuicMPConnectionTest(TestCase):
+    def check_handshake(self, client, server, alpn_protocol=None):
+        """
+        Check handshake completed.
+        """
+        event = client.next_event()
+        self.assertEqual(type(event), events.ProtocolNegotiated)
+        self.assertEqual(event.alpn_protocol, alpn_protocol)
+        event = client.next_event()
+        self.assertEqual(type(event), events.HandshakeCompleted)
+        self.assertEqual(event.alpn_protocol, alpn_protocol)
+        self.assertEqual(event.early_data_accepted, False)
+        self.assertEqual(event.session_resumed, False)
+        for i in range(7):
+            self.assertEqual(type(client.next_event()), events.ConnectionIdIssued)
+        for i in range(7):
+            self.assertEqual(type(client.next_event()), events.MPConnectionIdIssued)
+        self.assertIsNone(client.next_event())
+
+        event = server.next_event()
+        self.assertEqual(type(event), events.ProtocolNegotiated)
+        self.assertEqual(event.alpn_protocol, alpn_protocol)
+        event = server.next_event()
+        self.assertEqual(type(event), events.HandshakeCompleted)
+        self.assertEqual(event.alpn_protocol, alpn_protocol)
+        for i in range(7):
+            self.assertEqual(type(server.next_event()), events.ConnectionIdIssued)
+        for i in range(7):
+            self.assertEqual(type(server.next_event()), events.MPConnectionIdIssued)
+        self.assertIsNone(server.next_event())
+
+    def test_connect(self):
+        with client_and_server() as (client, server):
+            # check handshake completed
+            self.check_handshake(client=client, server=server)
+
+            # check each endpoint has available connection IDs for each uniflow for the peer
+            for i in range(2):
+                self.assertEqual(
+                    sequence_numbers(client._sending_uniflows[i].cid_available),
+                    [1, 2, 3, 4, 5, 6, 7],
+                )
+                self.assertEqual(
+                    sequence_numbers(server._sending_uniflows[i].cid_available),
+                    [1, 2, 3, 4, 5, 6, 7],
+                )
+
+            # client closes the connection
+            client.close()
+            self.assertEqual(transfer(client, server), 1)
+
+            # check connection closes on the client side
+            client.handle_timer(client.get_timer())
+            event = client.next_event()
+            self.assertEqual(type(event), events.ConnectionTerminated)
+            self.assertEqual(event.error_code, QuicErrorCode.NO_ERROR)
+            self.assertEqual(event.frame_type, None)
+            self.assertEqual(event.reason_phrase, "")
+            self.assertIsNone(client.next_event())
+
+            # check connection closes on the server side
+            server.handle_timer(server.get_timer())
+            event = server.next_event()
+            self.assertEqual(type(event), events.ConnectionTerminated)
+            self.assertEqual(event.error_code, QuicErrorCode.NO_ERROR)
+            self.assertEqual(event.frame_type, None)
+            self.assertEqual(event.reason_phrase, "")
+            self.assertIsNone(server.next_event())
+
+            # check client log
+            client_log = client.configuration.quic_logger.to_dict()
+            self.assertGreater(len(client_log["traces"][0]["events"]), 20)
+
+            # check server log
+            server_log = server.configuration.quic_logger.to_dict()
+            self.assertGreater(len(server_log["traces"][0]["events"]), 20)
